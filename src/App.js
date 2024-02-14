35de968936346b863a1328151cddebdc45c3eddb
Rishat Osmonov <risatosmonov@gmail.com>
2024-02-14 19:05:05 +0600

    updated packeg lock json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import React from "react";
-import Navbar from "./components/Navbar";
 
 const App = () => {
 	return (
@@ -10,13 +8,3 @@ const App = () => {
 		</>
 	);
 };
-
-export default App;
-  return (
-    <>
-      <Navbar />
-    </>
-  );
-};
-
-export default App;
