cbf9a72da17128c7032fbbbfebbd644ed2bb3d03
Rishat Osmonov <risatosmonov@gmail.com>
2024-02-14 18:59:13 +0600

    some commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+	return (
+		<>
+			<div className="header">
+				<div className="header__logo">
+					Roromp<span>ok</span>
+				</div>
+			</div>
+		</>
+	);
+}
